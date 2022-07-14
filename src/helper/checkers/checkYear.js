d17cd16cfba100ab00a0d9258b5fb2df5245fca9
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-07-14 15:11:36 +0300

    Fix stack

--- src/helper/checkers/checkYear.js
+++ src/helper/checkers/checkYear.js
@@ -3,6 +3,7 @@ import random from 'helper/random';
 export default function checkYear(year, data) {
   const yearSum = year.reduce((acc, year) => acc + year, 0);
   const midYear = yearSum / year.length;
+  console.log(midYear);
 
   if (midYear > data.minAverageAge && midYear < data.maxAverageAge) {
     return year;
